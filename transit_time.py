cb533513393ca35d553c185e2d838dbae1ad20d6
JJ-in-Jersey <jason.bronfeld@pm.me>
2025-07-01 13:25:23 -0400

    completely refactored arc process

--- transit_time.py
+++ transit_time.py
@@ -1,18 +1,18 @@
 from scipy.signal import savgol_filter
-from datetime import timedelta
-from dateparser import parse as parsedate
+from datetime import timedelta, datetime
 from pathlib import Path
-from pandas import concat, to_datetime, Series
+from pandas import concat, to_datetime, Series, Timestamp
 from numpy import ndarray
+from typing import Hashable
 
 from tt_dataframe.dataframe import DataFrame
 from tt_file_tools.file_tools import print_file_exists
-from tt_geometry.geometry import Arc
+from tt_geometry.geometry import Arc, StartArc, EndArc
 from tt_job_manager.job_manager import Job
 from tt_date_time_tools.date_time_tools import hours_mins
 from tt_gpx.gpx import Route, Segment
 from tt_globals.globals import PresetGlobals as pg
-from tt_exceptions.exceptions import IndexGreaterThanThree
+from tt_exceptions.exceptions import DateMismatch, LengthMismatch
 
 
 class TimeStepsFrame(DataFrame):
@@ -34,7 +34,8 @@ class TimeStepsFrame(DataFrame):
         ets_path = route.filepath('elapsed timesteps', speed)
 
         if tts_path.exists():
-            frame = DataFrame(csv_source=tts_path)
+            super().__init__(data=DataFrame(csv_source=tts_path))
+            self.Time = to_datetime(self.Time, utc=True)
         else:
             if not ets_path.exists():
                 raise FileExistsError(ets_path)
@@ -51,7 +52,7 @@ class TimeStepsFrame(DataFrame):
             frame['t_time'] = Series(transit_timesteps_arr)
             frame.write(tts_path)
 
-        super().__init__(data=frame)
+            super().__init__(data=frame)
 
 
 class TimeStepsJob(Job):
@@ -76,6 +77,7 @@ class SavGolFrame(DataFrame):
 
         if savgol_path.exists():
             super().__init__(data=DataFrame(csv_source=savgol_path))
+            self.Time = to_datetime(self.Time, utc=True)
         else:
             timesteps_frame['midline'] = savgol_filter(timesteps_frame.t_time, self.savgol_size, self.savgol_order).round().astype('int')
             timesteps_frame = timesteps_frame[timesteps_frame.t_time.ne(timesteps_frame.midline)].copy()  # remove values that equal the midline
@@ -108,24 +110,27 @@ class MinimaFrame(DataFrame):
 
         if minima_path.exists():
             super().__init__(data=DataFrame(csv_source=minima_path))
+            for col in [c for c in self.columns if 'time' in c]:
+                self[col] = self[col].apply(to_datetime)
         else:
             # create a list of minima frames (TF = True, below midline) and larger than the noise threshold
             blocks = [df.reset_index(drop=True).drop(labels=['GL', 'block', 'midline'], axis=1)
                   for index, df in savgol_frame.groupby('block') if df['GL'].any() and len(df) > MinimaFrame.noise_threshold]
 
-            frame = DataFrame(columns=['start_eastern_round', 'min_eastern_round', 'end_eastern_round'])
+            frame = DataFrame(columns=['start_time', 'min_time', 'end_time', 'start_duration', 'min_duration', 'end_duration'])
             for i, df in enumerate(blocks):
                 median_stamp = df[df.t_time == df.min().t_time]['stamp'].median().astype(int)
                 frame.at[i, 'start_utc'] = df.iloc[0].Time
-                frame.at[i, 'end_utc'] = df.iloc[-1].Time
                 frame.at[i, 'min_utc'] = df.iloc[abs(df.stamp - median_stamp).idxmin()].Time
-                frame.at[i, 'start_tt'] = hours_mins(df.iloc[0].t_time * pg.timestep)
-                frame.at[i, 'end_tt'] = hours_mins(df.iloc[-1].t_time * pg.timestep)
-                frame.at[i, 'min_tt'] = hours_mins(df.iloc[abs(df.stamp - median_stamp).idxmin()].t_time * pg.timestep)
+                frame.at[i, 'end_utc'] = df.iloc[-1].Time
+                frame.at[i, 'start_duration'] = hours_mins(df.iloc[0].t_time * pg.timestep)
+                frame.at[i, 'min_duration'] = hours_mins(df.iloc[abs(df.stamp - median_stamp).idxmin()].t_time * pg.timestep)
+                frame.at[i, 'end_duration'] = hours_mins(df.iloc[-1].t_time * pg.timestep)
 
-            frame['start_eastern_round'] =  to_datetime(frame.start_utc, utc=True).dt.tz_convert('US/Eastern').round('15min')
-            frame['min_eastern_round'] = to_datetime(frame.min_utc, utc=True).dt.tz_convert('US/Eastern').round('15min')
-            frame['end_eastern_round'] = to_datetime(frame.end_utc, utc=True).dt.tz_convert('US/Eastern').round('15min')
+            # all eastern timezone rounded to 15 minutes, for now!
+            frame.start_time =  to_datetime(frame.start_utc, utc=True).dt.tz_convert('US/Eastern').round('15min')
+            frame.min_time = to_datetime(frame.min_utc, utc=True).dt.tz_convert('US/Eastern').round('15min')
+            frame.end_time = to_datetime(frame.end_utc, utc=True).dt.tz_convert('US/Eastern').round('15min')
 
             frame.drop(['start_utc', 'min_utc', 'end_utc'], axis=1, inplace=True)
             frame.write(minima_path)
@@ -141,95 +146,80 @@ class MinimaJob(Job):  # super -> job name, result key, function/object, argumen
     def __init__(self, savgol_frame: DataFrame, speed: int, route: Route):
         job_name = 'minima' + ' ' + str(speed)
         result_key = speed
-        arguments = tuple([speed, route])
         arguments = tuple([savgol_frame, route.filepath('minima', speed)])
         super().__init__(job_name, result_key, MinimaFrame, arguments)
 
 
-def index_arc_df(frame):
-    output_frame = DataFrame(columns=['idx'] + frame.columns.to_list())
+class ArcsFrame(DataFrame):
 
-    date_arr_dict = {key: [] for key in sorted(list(set(frame['date'])))}
+    @staticmethod
+    def create_subordinate_arc(arc_class: type[StartArc] | type[EndArc], row_dict: dict, index: Hashable):
+        try:
+            return arc_class(**row_dict)
+        except LengthMismatch as lmm:
+            print(index, lmm)
+            return None
+        except Exception as e:
+            print(index, e)
+            return None
 
-    for i, row in frame.iterrows():
-        date_arr_dict[row['date']].append(row)
+    def __init__(self, minima_frame: DataFrame, arc_path: Path, speed: int, first_day: datetime.date, last_day: datetime.date):
 
-    for key in date_arr_dict.keys():
-        for i in range(len(date_arr_dict[key])):
-            output_frame.loc[len(output_frame)] = [i + 1] + date_arr_dict[key][i].tolist()
-        if len(date_arr_dict[key]) == 2:
-            last_row_dict = output_frame.loc[len(output_frame) - 1].to_dict()
-            new_dict = {key: None for key in last_row_dict}
-            new_dict.update({'idx': 3, 'date': last_row_dict['date'], 'speed': last_row_dict['speed']})
-            output_frame.loc[len(output_frame)] = new_dict
+        if arc_path.exists():
+            super().__init__(DataFrame(csv_source=arc_path))
+            for col in [c for c in self.columns if 'time' in c]:
+                self[col] = self[col].apply(to_datetime)
+        else:
+            arcs = []
+            for i, row in minima_frame.iterrows():
+                row_dict = row.to_dict()
+                try:
+                    arcs.append(Arc(**row_dict))
+                except DateMismatch:
+                    start_arc = self.create_subordinate_arc(StartArc, row_dict, i)
+                    if start_arc is not None:
+                        arcs.append(start_arc)
 
-    if len(output_frame[output_frame.idx > 3]):
-        raise IndexGreaterThanThree('Index greater than 3 for speed ' + str(frame.speed[0]))
+                    end_arc = self.create_subordinate_arc(EndArc, row_dict, i)
+                    if end_arc is not None:
+                        arcs.append(end_arc)
+                except LengthMismatch as lmm:
+                    print(i, lmm)
+                except Exception as e:
+                    print(i, e)
 
-    return output_frame
+            frame = DataFrame([arc.arc_dict for arc in arcs])
+            frame.insert(0, 'date', frame.start_time.apply(lambda timestamp: timestamp.date()))
+            frame = frame.sort_values(by=['date', 'start_time']).reset_index(drop=True)
+            frame.insert(0, 'idx', frame.groupby('date').cumcount() + 1)
+            frame.insert(1, 'speed', speed)
 
+            frame = frame[(frame['date'] >= first_day) & (frame['date'] <= last_day)]
+            frame.reset_index(drop=True, inplace=True)
 
-def create_arcs(minima_path: Path, speed: int):
-    minima_frame = read_df(minima_path)
-    for col in MinimaFrame.frame_time_columns:
-        minima_frame[col] = to_datetime(minima_frame[col])
-    start_year = to_datetime(minima_frame.start_eastern[0]).year
-    first_day = parsedate(Route.template_dict['first_day'].substitute({'year': start_year}))
-    last_day = parsedate(Route.template_dict['last_day'].substitute({'year': start_year+2}))
+            frame.write(arc_path)
+            super().__init__(frame)
 
-    arcs = [Arc(row.to_dict()) for i, row in minima_frame.iterrows()]
-    next_day_arcs = [arc.next_day_arc for arc in arcs if arc.next_day_arc]
-    all_arcs = arcs + next_day_arcs
-    all_good_arcs = [arc for arc in all_arcs if not arc.zero_angle]
-
-    arcs_df = DataFrame(columns=Arc.columns)
-    for d in [a.arc_dict for a in all_good_arcs]:
-        arcs_df.loc[len(arcs_df)] = d
-
-    # noinspection PyTypeChecker
-    arcs_df.insert(loc=0, column='date', value=None)
-    arcs_df['date'] = arcs_df.start_eastern.apply(lambda x: x.date())
-    arcs_df['speed'] = speed
-
-    arcs_df.sort_values(by=['start_eastern'], inplace=True)
-    arcs_df = index_arc_df(arcs_df)
-    arcs_df = arcs_df[arcs_df.date <= last_day.date()]
-    arcs_df = arcs_df[arcs_df.date >= first_day.date()]
-    arcs_df.reset_index(drop=True, inplace=True)
-
-    for col in ['start_round', 'min_round', 'end_round']:
-        arcs_df['str_' + col] = None
-        for row in range(len(arcs_df)):
-            arcs_df.loc[row, 'str_' + col] = arcs_df.loc[row, col].strftime("%I:%M %p") if arcs_df.loc[row, col] is not None else None
-
-    return arcs_df
+    # for col in ['start_round', 'min_round', 'end_round']:
+    #     arcs_df['str_' + col] = None
+    #     for row in range(len(arcs_df)):
+    #         arcs_df.loc[row, 'str_' + col] = arcs_df.loc[row, col].strftime("%I:%M %p") if arcs_df.loc[row, col] is not None else None
 
 
-class TransitTimeArcsFrame:
-
-    def __init__(self, speed: int, route: Route):
-
-        minima_path = route.filepath('minima', speed)
-        if not print_file_exists(minima_path):
-            raise FileExistsError(minima_path)
-
-        arcs_path = route.filepath('arcs', speed)
-        if not print_file_exists(arcs_path):
-            self.frame = create_arcs(minima_path, speed)
-            print_file_exists(write_df(self.frame, arcs_path))
-
-
-class TransitTimeArcsJob(Job):  # super -> job name, result key, function/object, arguments
+class ArcsJob(Job):  # super -> job name, result key, function/object, arguments
 
     def execute(self): return super().execute()
     def execute_callback(self, result): return super().execute_callback(result)
     def error_callback(self, result): return super().error_callback(result)
 
-    def __init__(self, speed: int, route: Route):
+    def __init__(self, minima_frame: DataFrame, route: Route, speed: int):
         job_name = 'arcs' + ' ' + str(speed)
         result_key = speed
-        arguments = tuple([speed, route])
-        super().__init__(job_name, result_key, TransitTimeArcsFrame, arguments)
+        year = minima_frame.loc[0]['start_time'].year
+        first_day = to_datetime(Route.template_dict['first_day'].substitute({'year': year})).date()
+        last_day = to_datetime(Route.template_dict['last_day'].substitute({'year': year+2})).date()
+        arguments = tuple([minima_frame, route.filepath('arcs', speed), speed, first_day, last_day])
+        super().__init__(job_name, result_key, ArcsFrame, arguments)
 
 
 def transit_time_processing(job_manager, route: Route):
@@ -248,9 +238,9 @@ def transit_time_processing(job_manager, route: Route):
     keys = [job_manager.submit_job(MinimaJob(frame, speed, route)) for speed, frame in results.items()]
     job_manager.wait()
 
-    print(f'\nCalculating transit arcs')
-    for speed in pg.speeds:
-        job_manager.submit_job(TransitTimeArcsJob(speed, route))
+    print(f'\nGenerating arcs frame')
+    results = {speed: job_manager.get_result(speed) for speed in keys}  # {'speed': frame}
+    keys = [job_manager.submit_job(ArcsJob(frame, route, speed)) for speed, frame in results.items()]
     job_manager.wait()
 
     print(f'\nAggregating transit times', flush=True)
