dd0587c5f88f28b44663f81625d1be8d928e1345
jj-in-jersey <faircurrents@gmail.com>
2025-10-17 22:16:09 -0400

    added new fc_globals; imported jobs instead of each job; created separate result lists for jobs; in main.py

--- main.py
+++ main.py
@@ -5,13 +5,12 @@ import numpy as np
 from pandas import merge, to_datetime, concat
 
 from tt_dataframe.dataframe import DataFrame
-from tt_globals.globals import PresetGlobals
+import tt_globals.globals as fc_globals
 from tt_gpx.gpx import Route, EdgeNode, GpxFile, Segment
 from tt_job_manager.job_manager import JobManager
+import tt_jobs.jobs as jobs
 from tt_noaa_data.noaa_data import StationDict
 from tt_file_tools.file_tools import print_file_exists
-
-from tt_jobs.jobs import ElapsedTimeJob, TimeStepsJob, SavGolJob, SavGolFrame, FairCurrentJob, SavGolMinimaJob, FairCurrentMinimaJob, ArcsJob
 
 if __name__ == '__main__':
 
@@ -20,16 +19,16 @@ if __name__ == '__main__':
     args = vars(ap.parse_args())
 
     # ---------- ROUTE OBJECT ----------
-    if PresetGlobals.stations_file.exists():
-        station_dict = StationDict(json_source=PresetGlobals.stations_file)
+    if fc_globals.STATIONS_FILE.exists():
+        station_dict = StationDict(json_source=fc_globals.STATIONS_FILE)
     else:
         station_dict = StationDict()
     gpx_file = GpxFile(args['filepath'])
     route = Route(station_dict, gpx_file.tree)
 
     print(f'\nParameters')
-    print(f'savgol window size {SavGolFrame.savgol_size}')
-    print(f'savgol polynomial order {SavGolFrame.savgol_order}')
+    print(f'savgol window size {jobs.SavGolFrame.savgol_size}')
+    print(f'savgol polynomial order {jobs.SavGolFrame.savgol_order}')
 
     print(f'\nCalculating route {route.name}')
     print(f'code {route.code}')
@@ -44,7 +43,7 @@ if __name__ == '__main__':
             node_list.append(node.next_edge.length)
         node_list.append(segment.node_list[-1].name)
         print(f'{segment.name}: {node_list} {int(segment.length*100)/100} nm')
-    print(f'boat speeds: {PresetGlobals.speeds}')
+    print(f'boat speeds: {fc_globals.SPEEDS}')
     print(f'length {int(route.length*100)/100} nm')
     print(f'direction: {route.directions[0]}, abbrev: {route.dir_abbrevs[0]},  heading: {route.heading}\n')
 
@@ -73,10 +72,9 @@ if __name__ == '__main__':
 
     print(f'\nCalculating elapsed times')
     results = {}
-    for speed in PresetGlobals.speeds:
+    for speed in fc_globals.SPEEDS:
         print(f'Edges at {speed} kts')
-
-        ets_path = route.filepath('elapsed timesteps', speed)
+        ets_path = route.filepath(jobs.ElapsedTimeFrame.__name__, speed)
         if ets_path.exists():
             elapsed_time_df = DataFrame(csv_source=ets_path)
             seg_cols = [c for c in elapsed_time_df.columns if Segment.prefix in c]
@@ -87,7 +85,7 @@ if __name__ == '__main__':
             print()
             elapsed_time_df.Time = to_datetime(elapsed_time_df.Time, utc=True)
         else:
-            keys = [job_manager.submit_job(ElapsedTimeJob(seg, speed)) for seg in route.segments]
+            keys = [job_manager.submit_job(jobs.ElapsedTimeJob(seg, speed)) for seg in route.segments]
             job_manager.wait()
 
             print(f'      Aggregating elapsed timesteps at {speed} kts into a dataframe', flush=True)
@@ -100,77 +98,54 @@ if __name__ == '__main__':
         results[speed] = elapsed_time_df
 
     print(f'\nCalculating transit times')
-    keys = [job_manager.submit_job(TimeStepsJob(frame, speed, route)) for speed, frame in results.items()]
+    keys = [job_manager.submit_job(jobs.TimeStepsJob(frame, speed, route)) for speed, frame in results.items()]
     job_manager.wait()
+    results_transit_times_frame = {speed: job_manager.get_result(speed) for speed in keys}  # {'speed': frame}
 
     print(f'\nGenerating fair current frames')
-    results = {speed: job_manager.get_result(speed) for speed in keys}  # {'speed': frame}
-    keys = [job_manager.submit_job(FairCurrentJob(frame, speed, route)) for speed, frame in results.items()]
+    keys = [job_manager.submit_job(jobs.FairCurrentJob(frame, speed, route)) for speed, frame in results_transit_times_frame.items()]
+    # for speed, frame in results.items():
+    #     job = jobs.FairCurrentJob(frame, speed, route)
+    #     results = job.execute()
     job_manager.wait()
+    results_fair_currents_frame = {speed: job_manager.get_result(speed) for speed in keys}  # {'speed': frame}
 
     print(f'\nGenerating savgol frames')
-    results = {speed: job_manager.get_result(speed) for speed in keys}  # {'speed': frame}
-    keys = [job_manager.submit_job(SavGolJob(frame, speed, route)) for speed, frame in results.items()]
+    keys = [job_manager.submit_job(jobs.SavGolJob(frame, speed, route)) for speed, frame in results_transit_times_frame.items()]
+    # for speed, frame in results.items():
+    #     job = jobs.SavGolJob(frame, speed, route)
+    #     result = job.execute()
     job_manager.wait()
+    results_savgol_frame = {speed: job_manager.get_result(speed) for speed in keys}  # {'speed': frame}
 
     print(f'\nGenerating fair current minima frames')
-    results = {speed: job_manager.get_result(speed) for speed in keys}  # {'speed': frame}
-    keys = [job_manager.submit_job(FairCurrentMinimaJob(frame, speed, route)) for speed, frame in results.items()]
+    keys = [job_manager.submit_job(jobs.FairCurrentMinimaJob(frame, speed, route)) for speed, frame in results_fair_currents_frame.items()]
     # for speed, frame in results.items():
     #     job = MinimaJob(frame, speed, route)
     #     result = job.execute()
     job_manager.wait()
+    results_fair_current_minima_frame = {speed: job_manager.get_result(speed) for speed in keys}  # {'speed': frame}
 
     print(f'\nGenerating savgol minima frames')
-    results = {speed: job_manager.get_result(speed) for speed in keys}  # {'speed': frame}
-    keys = [job_manager.submit_job(SavGolMinimaJob(frame, speed, route)) for speed, frame in results.items()]
-    # for speed, frame in results.items():
-    #     job = MinimaJob(frame, speed, route)
+    keys = [job_manager.submit_job(jobs.SavGolMinimaJob(frame, speed, route)) for speed, frame in results_savgol_frame.items()]
+    # for speed, frame in results_savgol_frame.items():
+    #     job = jobs.SavGolMinimaJob(frame, speed, route)
     #     result = job.execute()
     job_manager.wait()
+    results_savgol_minima_frame = {speed: job_manager.get_result(speed) for speed in keys}  # {'speed': frame}
 
     print(f'\nGenerating arcs frame')
-    results = {speed: job_manager.get_result(speed) for speed in keys}  # {'speed': frame}
-    keys = [job_manager.submit_job(ArcsJob(frame, route, speed)) for speed, frame in results.items()]
+    keys = [job_manager.submit_job(jobs.ArcsJob(frame, route, speed)) for speed, frame in results.items()]
     job_manager.wait()
 
     print(f'\nAggregating transit times', flush=True)
-    frames = [DataFrame(csv_source=route.filepath('arcs', speed)) for speed in PresetGlobals.speeds]
+    frames = [DataFrame(csv_source=route.filepath('arcs', speed)) for speed in fc_globals.SPEEDS]
     transit_times_df = concat(frames).reset_index(drop=True)
     transit_times_df = transit_times_df.fillna("-")
     transit_times_df.drop(['start_datetime', 'min_datetime', 'end_datetime'], axis=1, inplace=True)
 
-    transit_times_path = route.folder.joinpath(route.template_dict['transit times'].substitute({'loc': route.code}))
+    transit_times_path = route.folder.joinpath(fc_globals.TEMPLATES['transit_times'].substitute({'loc': route.code}))
     print_file_exists(transit_times_df.write(transit_times_path))
-
-
-    # # if args['east_river']:
-    # #     print(f'\nEast River validation')
-    # #
-    # #     validation_frame = pd.DataFrame()
-    # #
-    # #     frame = validations.hell_gate_validation()
-    # #     validation_frame = pd.concat([validation_frame, frame])
-    # #
-    # #     frame = validations.battery_validation()
-    # #     validation_frame = pd.concat([validation_frame, frame])
-    # #
-    # #     validation_frame.sort_values(['start_date'], ignore_index=True, inplace=True)
-    # #     write_df(validation_frame, Globals.TRANSIT_TIMES_FOLDER.joinpath('east_river_validation.csv'))
-    # #
-    # # if args['chesapeake_delaware_canal']:
-    # #     print(f'\nChesapeake Delaware Canal validation')
-    # #
-    # #     validation_frame = pd.DataFrame()
-    # #
-    # #     frame = validations.chesapeake_city_validation()
-    # #     validation_frame = pd.concat([validation_frame, frame])
-    # #
-    # #     frame = validations.reedy_point_tower_validation()
-    # #     validation_frame = pd.concat([validation_frame, frame])
-    # #
-    # #     validation_frame.sort_values(['start_date'], ignore_index=True, inplace=True)
-    # #     write_df(validation_frame, Globals.TRANSIT_TIMES_FOLDER.joinpath('chesapeake_delaware_validation.csv'))
 
     print(f'\nProcess Complete')
 
