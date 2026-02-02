28c58647b2bab492091be18548822ade4ed33574
albatross <hercules.class011@passinbox.com>
2026-02-02 17:48:03 -0500

    added trim back to begining of the mogrify commands

--- bin/mogrify.bat
+++ bin/mogrify.bat
@@ -11,7 +11,7 @@ if not exist "%~1" (
 )
 
 echo Processing PNG files in %~1
-magick mogrify -fuzz 15%% -monitor -transparent "rgb(0,0,255)" -channel A -morphology Erode Disk:1 +channel -filter Lanczos -resize 900x -background none -alpha background -strip +repage "%~1/*.png"
+magick mogrify -trim -strip +repage -fuzz 15%% -monitor -transparent "rgb(0,0,255)" -channel A -morphology Erode Disk:1 +channel -filter Lanczos -resize 900x -background none -alpha background "%~1/*.png"
 echo Done!
 
-magick mogrify -fuzz 15%% -monitor -transparent "rgb(0,0,255)" -channel A -morphology Erode Disk:1 +channel -filter Lanczos -resize 900x -background none -alpha background -strip +repage *.png
+magick mogrify -trim -strip +repage -fuzz 15%% -monitor -transparent "rgb(0,0,255)" -channel A -morphology Erode Disk:1 +channel -filter Lanczos -resize 900x -background none -alpha background *.png
