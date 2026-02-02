23cc04b13b41c78205282ddbc73376de1957c021
albatross <hercules.class011@passinbox.com>
2026-02-02 01:07:57 -0500

    updated mogrify arguments to remove trim which isn't needed when you export slides rather than export current selection

--- bin/mogrify.bat
+++ bin/mogrify.bat
@@ -11,7 +11,7 @@ if not exist "%~1" (
 )
 
 echo Processing PNG files in %~1
-magick mogrify -fuzz 15%% -transparent "rgb(0,0,255)" -channel A -morphology Erode Disk:1 +channel -filter Lanczos -resize 900x -background none -alpha background -strip -trim +repage "%~1/*.png"
+magick mogrify -fuzz 15%% -monitor -transparent "rgb(0,0,255)" -channel A -morphology Erode Disk:1 +channel -filter Lanczos -resize 900x -background none -alpha background -strip +repage "%~1/*.png"
 echo Done!
 
-rem magick mogrify -fuzz 15% -transparent "rgb(0,0,255)" -channel A -morphology Erode Disk:1 +channel -filter Lanczos -resize 900x -background none -alpha background *.png
+magick mogrify -fuzz 15%% -monitor -transparent "rgb(0,0,255)" -channel A -morphology Erode Disk:1 +channel -filter Lanczos -resize 900x -background none -alpha background -strip +repage *.png
