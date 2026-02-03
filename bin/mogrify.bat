5abdb31dfd0fc0b5acec995ed510378039c6ae84
JJ-in-Jersey <jason.bronfeld@pm.me>
2026-02-03 11:48:53 -0500

    fixed %% in cut and paste line

--- bin/mogrify.bat
+++ bin/mogrify.bat
@@ -14,4 +14,4 @@ echo Processing PNG files in %~1
 magick mogrify -monitor -trim -strip +repage -fuzz 15%% -monitor -transparent "rgb(0,0,255)" -channel A -morphology Erode Disk:1 +channel -filter Lanczos -resize 900x -background none -alpha background "%~1/*.png"
 echo Done!
 
-magick mogrify -monitor -trim -strip +repage -fuzz 15%% -monitor -transparent "rgb(0,0,255)" -channel A -morphology Erode Disk:1 +channel -filter Lanczos -resize 900x -background none -alpha background *.png
+magick mogrify -monitor -trim -strip +repage -fuzz 15% -monitor -transparent "rgb(0,0,255)" -channel A -morphology Erode Disk:1 +channel -filter Lanczos -resize 900x -background none -alpha background *.png
